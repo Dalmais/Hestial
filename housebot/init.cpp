0a0f26e04abbb773afc21ab349172f2c9646cdcb
Romain <romain@macbook-air-de-marie.local>
2018-08-30 07:54:41 +0200

    Add init files

---
+++ housebot/init.cpp
@@ -0,0 +1,9 @@
+//
+//  init.cpp
+//  housebot
+//
+//  Created by Romain on 30.08.18.
+//  Copyright © 2018 Romain Dalmais. All rights reserved.
+//
+
+#include "init.hpp"
---
+++ housebot/init.hpp
@@ -0,0 +1,17 @@
+//
+//  init.hpp
+//  housebot
+//
+//  Created by Romain on 30.08.18.
+//  Copyright © 2018 Romain Dalmais. All rights reserved.
+//
+
+#ifndef _INIT_
+#define _INIT_
+
+namespace housebot{
+    bool initCommon();
+    bool initDrivers();
+}
+
+#endif /* _INIT_ */
