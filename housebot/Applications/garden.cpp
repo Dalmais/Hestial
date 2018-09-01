f59068b30018484f772257b86db9d973defdcf04
Dalmais <dalmais@gmail.com>
2018-09-01 09:49:14 +0200

    Add drivers directory and Application
    Create :
    -Drivers directory
    - Application directory
    - Garden class

---
+++ housebot/Applications/garden.cpp
@@ -0,0 +1,18 @@
+//
+//  garden.cpp
+//  housebot
+//
+//  Created by Romain on 01.09.18.
+//  Copyright © 2018 Romain Dalmais. All rights reserved.
+//
+
+#include "garden.hpp"
+
+
+class Garden : Application {
+public :
+    
+    
+private:
+
+};
