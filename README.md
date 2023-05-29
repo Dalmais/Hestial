69c2e1e3e59fd37ee8620d14adfedb2d7f5aec8f
Dalmais <dalmais@gmail.com>
2023-05-29 16:32:27 +0200

    Add copyright

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # Housebot
-Bot for house management
+This is a bunch of application/bot for the house management.
+This currently contain a superviser application and a bunch of tools for gardening
+Futurs applications are like a camera monitoring services son 
 
-Using #spdlog for log.
+## Homebase
+This application collect data from the different connected devices. 
+These device can be from housebot or from other devices with a supported open protocol
+Open supported protocol are : None
+It also feed data for a web server in order to display this information on a browser.
+
+## Webpage
+
+
+## HomeGarden
+
+
+##
--- src/Homegarden/main.cpp
+++ src/Homegarden/main.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2023 Romain Damais (dalmais<at>gmail.com)
+// See end of file for extended copyright information
+
 #include <iostream>
 
 using namespace std;
@@ -7,3 +10,20 @@ int main()
     cout << "Hello World!" << endl;
     return 0;
 }
+
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
